dc1b861d3d54a6ee3c9fc5513a1cd9aab54d3979
he8079 <105124202+he8079@users.noreply.github.com>
2022-07-15 11:19:00 -0400

    Update js

--- script.js
+++ script.js
@@ -68,8 +68,8 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+//  slides[slideIndex - 1].style.display = "block";
+//  dots[slideIndex - 1].className += " active";
 }
 
 // Add the novalidate attribute when the JS loads
